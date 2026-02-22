b193942d0f9b3939f4040ec1ac037671655a7c0d
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2026-02-22 19:03:03 -0300

    Update Filtro

--- Filtro.java
+++ Filtro.java
@@ -2,26 +2,26 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// public class Filtro {
-
-//     public static void main(String[] args) {
-
-//         List<Integer> valoresPares = Arrays.asList(1, 2, 3, 4, 5, 6);
-
-//         List<Integer> pares = valoresPares
-//                 .stream()
-//                 .filter(n -> n % 2 == 0)
-//                 .collect(Collectors.toList());
-
-//         System.out.println("Resultado: " + pares);
-//     }
-
-// }
-
-import java.util.Arrays;
-import java.util.List;
-
 public class Filtro {
+
+    public static void main(String[] args) {
+
+        List<Integer> valoresPares = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        List<Integer> pares = valoresPares
+                .stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println("Resultado: " + pares);
+    }
+
+}
+
+// import java.util.Arrays;
+// import java.util.List;
+
+ class Filtro2 {
     public static void main(String[] args) {
 
         List<Integer> numeros = Arrays.asList(1, 3, 5, 7, 9, 2, 4, 6);
